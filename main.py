98c97afec9248711842cc6a8c2a36e6ec5de9127
frsouzaa <felipe.souza1506@outlook.com>
2022-08-22 14:39:35 -0300

    Alteirei o algoritmo de movimentãção da cobra

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class Body(pygame.sprite.Sprite):
         self.body = pygame.Surface((30, 30))
         self.body.fill((80, 242, 112))
         self.rect = self.body.get_rect()
-        # self.rect.move_ip(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        self.direction = 1
 
 
 class Curva(pygame.sprite.Sprite):
@@ -32,6 +32,27 @@ class Curva(pygame.sprite.Sprite):
         self.rect = self.body.get_rect()
         self.rect.move_ip(10, 10)
         self.colliding = False
+
+
+def move():
+    global body
+    global pixels
+    for k in range(1, len(body)):
+        if (body[k].rect[1] > body[k - 1].rect[1] and body[k].rect[0] == body[k - 1].rect[0]) or \
+                (body[k].rect[1] > body[k - 1].rect[1] and body[k - 1].direction != 1):
+            body[k].rect.move_ip(0, -pixels)
+            body[k].direction = 1
+        elif (body[k].rect[1] < body[k - 1].rect[1] and body[k].rect[0] == body[k - 1].rect[0]) or \
+                (body[k].rect[1] < body[k - 1].rect[1] and body[k - 1].direction != 2):
+            body[k].rect.move_ip(0, pixels)
+            body[k].direction = 2
+        elif (body[k].rect[0] > body[k - 1].rect[0] and body[k].rect[1] == body[k - 1].rect[1]) or \
+                (body[k].rect[0] > body[k - 1].rect[0] and body[k - 1].direction != 3):
+            body[k].rect.move_ip(-pixels, 0)
+            body[k].direction = 3
+        else:
+            body[k].rect.move_ip(pixels, 0)
+            body[k].direction = 4
 
 
 pygame.init()
@@ -68,32 +89,32 @@ while running:
     pressedKeys = pygame.key.get_pressed()
 
     if pressedKeys[K_UP] and lastKey[0] != 2 and lastKey[0] != 1:
-        # x = body.rect[0] - ((body.rect[0]) // 30) * 30
         print("up")
+        body[0].direction = 1
         lastKey[0] = 1
         direction = 1
         curva.append(Curva())
         curva[-1].rect.update(body[0].rect[0], body[0].rect[1], 30, 30)
         screen.blit(curva[-1].body, curva[-1].rect)
     if pressedKeys[K_DOWN] and lastKey[0] != 1 and lastKey[0] != 2:
-        # x = body.rect[0] - ((body.rect[0]) // 30) * 30
         print("down")
+        body[0].direction = 2
         lastKey[0] = 2
         direction = 2
         curva.append(Curva())
         curva[-1].rect.update(body[0].rect[0], body[0].rect[1], 30, 30)
         screen.blit(curva[-1].body, curva[-1].rect)
     if pressedKeys[K_LEFT] and lastKey[0] != 4 and lastKey[0] != 3:
-        # x = body.rect[1] - ((body.rect[1])//30)*30
         print("left")
+        body[0].direction = 3
         lastKey[0] = 3
         direction = 3
         curva.append(Curva())
         curva[-1].rect.update(body[0].rect[0], body[0].rect[1], 30, 30)
         screen.blit(curva[-1].body, curva[-1].rect)
     if pressedKeys[K_RIGHT] and lastKey[0] != 3 and lastKey[0] != 4:
-        # x = body.rect[1] - ((body.rect[1])//30)*30
         print("right")
+        body[0].direction = 4
         lastKey[0] = 4
         direction = 4
         curva.append(Curva())
@@ -102,20 +123,16 @@ while running:
 
     if direction == 1:
         body[0].rect.move_ip(0, -pixels)
-        body[1].rect.update(body[0].rect[0], body[0].rect[1] + 30, 30, 30)
-        body[2].rect.update(body[0].rect[0], body[0].rect[1] + 60, 30, 30)
+        move()
     elif direction == 2:
         body[0].rect.move_ip(0, pixels)
-        body[1].rect.update(body[0].rect[0], body[0].rect[1] - 30, 30, 30)
-        body[2].rect.update(body[0].rect[0], body[0].rect[1] - 60, 30, 30)
+        move()
     elif direction == 3:
         body[0].rect.move_ip(-pixels, 0)
-        body[1].rect.update(body[0].rect[0] + 30, body[0].rect[1], 30, 30)
-        body[2].rect.update(body[0].rect[0] + 60, body[0].rect[1], 30, 30)
+        move()
     elif direction == 4:
         body[0].rect.move_ip(pixels, 0)
-        body[1].rect.update(body[0].rect[0] - 30, body[0].rect[1], 30, 30)
-        body[2].rect.update(body[0].rect[0] - 60, body[0].rect[1], 30, 30)
+        move()
 
     if body[0].rect[0] >= 781 or body[0].rect[0] <= 1 or body[0].rect[1] >= 631 or body[0].rect[1] <= 1:
         running = False
